317720b1f926cef9a5aaf982f7add84decba1ba6
DevVM <raphaelpda140@gmail.com>
2015-11-19 00:58:11 -0200

    +Created ajax helper. #Incresed token expiration time. +Created a separated view for oauth and socialite testing.

--- laravel/resources/views/welcome.blade.php
+++ laravel/resources/views/welcome.blade.php
@@ -40,26 +40,5 @@
                 <div class="title">Laravel 5</div>
             </div>
         </div>
-
-        <button class="providerLogin" data-url="{{url('')}}" data-provider="facebook" data-callback="showUserInfo">Facebook Login</button>
-
-        <button class="providerLogin" data-url="{{url('')}}" data-provider="google" data-callback="showUserInfo">Gmail login</button>
-
-        <button class="providerLogin" data-url="{{url('')}}" data-provider="github" data-callback="showUserInfo">GitHub login</button>
-
-        <div id="personalInfo">
-            <img id="personalFoto" src="" alt="">
-            <h2 id="personalName"></h2>
-        </div>
-
-
-        <script>
-            function showUserInfo(success, token){
-                console.log(success);
-                console.log(token);
-            }
-        </script>
-
-        <script type="text/javascript" src="{!!asset('build/js/third-party-login/loginMain.js')!!}"></script>
     </body>
 </html>
